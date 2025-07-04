dcb69d2ef44e6957900b66eac9fee7cfb9daa487
Bhavesh Kashikar <bhavesh.kashikar@outlook.com>
2025-07-04 15:34:24 +0530

    Added publish

--- CortekAI.Security.Service/CortekAI.Security.Service/Controllers/SecurityController.cs
+++ CortekAI.Security.Service/CortekAI.Security.Service/Controllers/SecurityController.cs
@@ -19,17 +19,7 @@ namespace CortekAI.Security.Service.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+       
 
         [HttpPost(Name ="Login")]
         public async Task<TokenResponse> Login(LoginRequest loginRequest)
--- CortekAI.Security.Service/CortekAI.Security.Service/obj/Debug/net8.0/CortekAI.Security.Service.AssemblyInfo.cs
+++ CortekAI.Security.Service/CortekAI.Security.Service/obj/Debug/net8.0/CortekAI.Security.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CortekAI.Security.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3be5cbcbf6a9613994223605a00bd1d7baa96f6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CortekAI.Security.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CortekAI.Security.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
